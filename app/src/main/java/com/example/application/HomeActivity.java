d86100a09b3b90d3d620e92132eca7c3ef67fe40
Onis <dsamerka@mail.ru>
2023-12-06 00:18:21 +0300

    processing new incidents

--- app/src/main/java/com/example/application/HomeActivity.java
+++ app/src/main/java/com/example/application/HomeActivity.java
@@ -3,34 +3,35 @@ package com.example.application;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
-import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.yandex.mapkit.Animation;
 import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
 import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.TextStyle;
 import com.yandex.mapkit.mapview.MapView;
 import com.yandex.mapkit.user_location.UserLocationLayer;
+import com.yandex.runtime.image.ImageProvider;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Objects;
 import java.util.Properties;
 
 public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     public String MAPKIT_API_KEY;
     private MapView mapView;
+    private Uri imageUri;
     BottomNavigationView bottomNavigationView;
 
     @Override
@@ -43,31 +44,47 @@ public class HomeActivity extends AppCompatActivity implements BottomNavigationV
         } catch (IOException e) {
             e.printStackTrace();
         }
-        MapKitFactory.setApiKey(MAPKIT_API_KEY);
-        MapKitFactory.initialize(this);
+        if (!hasUri()) {
+            MapKitFactory.setApiKey(MAPKIT_API_KEY);
+        }
         setContentView(R.layout.activity_home);
         super.onCreate(savedInstanceState);
+        MapKitFactory.initialize(this);
         init();
+
+        CameraPosition position = new CameraPosition(new Point(57.21, 41.90),
+                14, 0, 0);
         mapView.getMap().setNightModeEnabled(true);
         mapView.getMap().setRotateGesturesEnabled(false);
-        mapView.getMap().move(new CameraPosition(
-                        new Point(57.21, 41.90),
-                        14, 0, 0), new Animation(Animation.Type.SMOOTH, 1f),
-                null);
+        mapView.getMap().move(position, new Animation(Animation.Type.SMOOTH, 1f), null);
         if (requestLocationPermission()) {
             MapKit mapKit = MapKitFactory.getInstance();
             UserLocationLayer locationLayer = mapKit.createUserLocationLayer(mapView.getMapWindow());
             locationLayer.setVisible(true);
             locationLayer.setHeadingEnabled(true);
         }
-        bottomNavigationView
-                .setOnNavigationItemSelectedListener(this);
+        if (hasUri()) {
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Bitmap finalBitmap = bitmap;
+            assert finalBitmap != null;
+            bitmap.setHasMipMap(true);
+            mapView.getMap().getMapObjects().addPlacemark(object -> {
+                object.setGeometry(position.getTarget());
+                object.setIcon(ImageProvider.fromBitmap(finalBitmap));
+                object.setText("Тест", new TextStyle().setPlacement(TextStyle.Placement.BOTTOM));
+            });
+        }
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.home);
     }
 
     private void init() {
-        bottomNavigationView
-                = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
         mapView = findViewById(R.id.mapview);
     }
 
@@ -132,5 +149,14 @@ public class HomeActivity extends AppCompatActivity implements BottomNavigationV
                 return true;
         }
         return false;
+    }
+
+    private boolean hasUri() {
+        String uri = getIntent().getStringExtra("imageUri");
+        if (uri != null) {
+            this.imageUri = Uri.parse(uri);
+            return true;
+        } else
+            return false;
     }
 }
--- app/src/main/java/com/example/application/IncidentFragment.java
+++ app/src/main/java/com/example/application/IncidentFragment.java
@@ -1,18 +1,22 @@
 package com.example.application;
 
+import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationManager;
-import android.graphics.drawable.Icon;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Button;
+import android.provider.MediaStore;
+import android.widget.*;
+import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.android.material.textfield.TextInputEditText;
 
+import static android.app.Activity.RESULT_OK;
 import static android.content.Context.NOTIFICATION_SERVICE;
 
 /**
@@ -26,7 +30,13 @@ public class IncidentFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private static final int GALLERY_REQ_CODE = 1000;
+    private Button redButton, materialButton;
+    private ImageView image;
+    private TextView textView3;
+    private CheckBox checkBox;
+    private TextInputEditText editTextTextPersonName;
+    private Uri uri;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -62,11 +72,12 @@ public class IncidentFragment extends Fragment {
         }
     }
 
+    @SuppressLint("WrongConstant")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         this.inflatedView =  inflater.inflate(R.layout.fragment_incident, container, false);
-        Button redButton = inflatedView.findViewById(R.id.button);
+        init();
         redButton.setOnClickListener(v -> {
             NotificationCompat.Builder builder =
                     new NotificationCompat.Builder(inflater.getContext())
@@ -77,7 +88,47 @@ public class IncidentFragment extends Fragment {
             NotificationManager notificationManager =
                     (NotificationManager) inflatedView.getContext().getSystemService(NOTIFICATION_SERVICE);
             notificationManager.notify(1, notification);
+
+            if (materialButton.getVisibility() != 0) {
+                if (!editTextTextPersonName.getEditableText().toString().equals("")) {
+                    if (checkBox.isChecked()) {
+                        Intent intent = new Intent(this.getContext(), HomeActivity.class);
+                        intent.putExtra("imageUri", uri.toString());
+                        startActivity(intent);
+                    } else
+                        Toast.makeText(this.getContext(), "Подтвердите согласие на обработку данных", Toast.LENGTH_LONG).show();
+                } else
+                    Toast.makeText(this.getContext(), "Заполните поля", Toast.LENGTH_LONG).show();
+            } else
+                Toast.makeText(this.getContext(), "Материал не загружен", Toast.LENGTH_LONG).show();
+        });
+        materialButton.setOnClickListener(v -> {
+            Intent gallery = new Intent(Intent.ACTION_PICK);
+            gallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(gallery, GALLERY_REQ_CODE);
         });
         return inflatedView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == GALLERY_REQ_CODE) {
+            assert data != null;
+            this.uri = data.getData();
+            image.setVisibility(View.VISIBLE);
+            materialButton.setVisibility(View.INVISIBLE);
+            textView3.setText("1. Материал загружен");
+            image.setImageURI(uri);
+        }
+    }
+
+    public void init() {
+        checkBox = inflatedView.findViewById(R.id.checkBox);
+        textView3 = inflatedView.findViewById(R.id.textView3);
+        redButton = inflatedView.findViewById(R.id.button);
+        materialButton = inflatedView.findViewById(R.id.button2);
+        image = inflatedView.findViewById(R.id.image);
+        editTextTextPersonName = inflatedView.findViewById(R.id.editTextTextPersonName);
     }
 }
